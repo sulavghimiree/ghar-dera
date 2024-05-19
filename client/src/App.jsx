d11ea6f5ff4e49295dbdcaa50d97069a44765c7e
Sulav Ghimire <sulavg2003@gmail.com>
2024-05-19 19:58:31 +0545

    Frontend started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,54 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/navbar/Navbar";
+import Footer from "./Components/footer/Footer";
+import Hero from "./Components/hero/Hero";
+import PopularRooms from "./Components/popularRooms/PopularRooms";
+import Newsletter from "./Components/newsletter/Newsletter";
+import Signup from "./Components/signup/Signup";
+import Login from "./Components/login/Login";
+import Rooms from "./Components/rooms/Rooms";
+import RoomDetails from "./Components/roomDetails/RoomDetails";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <PopularRooms />
+              <Newsletter />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/rooms"
+          element={
+            <>
+              <Navbar />
+              <Rooms />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/roomDetails"
+          element={
+            <>
+              <Navbar />
+              <RoomDetails />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
