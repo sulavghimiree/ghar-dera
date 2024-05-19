51d189f30ca9519ced3f2a180a7e3a2c4d3ca681
Sulav Ghimire <sulavg2003@gmail.com>
2024-05-19 22:12:59 +0545

    navbar finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           }
         />
         <Route
-          path="/roomDetails"
+          path="/roomDetails/:id"
           element={
             <>
               <Navbar />
--- client/src/Components/navbar/Navbar.jsx
+++ client/src/Components/navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { BsHouseDoor } from "react-icons/bs";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <Link to="/" className="left">
+          Ghar Dera <BsHouseDoor />
+        </Link>
+        <ul className="center">
+          <li className="listItem">Home</li>
+          <li className="listItem">About</li>
+          <li className="listItem">Popular</li>
+          <li className="listItem">Contact</li>
+        </ul>
+        <div className="right">
+          <Link to="/signup">Sign up</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
