f0b385334e2c71948999ebcedb91889dd9c5f2aa
Sulav Ghimire <sulavg2003@gmail.com>
2024-05-21 11:04:42 +0545

    final

--- client/src/Components/popularRooms/PopularRooms.jsx
+++ client/src/Components/popularRooms/PopularRooms.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./PopularRooms.css";
+import classes from "./PopularRooms.module.css";
 
 const PopularRooms = () => {
-  return <div>PopularRooms</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <div className={classes.title}>
+          <h5>Different types of Properties</h5>
+          <h2>Best types of properties for you</h2>
+        </div>
+        <div className={classes.properties}>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PopularRooms;
